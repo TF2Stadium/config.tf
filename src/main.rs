c5de63ee98df5e922282168878d0a0d22755b1ab
gpittarelli <tf2stadium@gjp.cc>
2016-11-12 16:13:43 -0800

    Setup router + index.html

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 extern crate iron;
+extern crate router;
 
 use iron::prelude::*;
 use iron::status;
+use router::Router;
+
+static INDEX: &'static [u8] = include_bytes!("index.html");
 
 fn main() {
-    fn hello_world(r: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, format!("Hello World! {}", r.url))))
+    let mut router = Router::new();
+    router.get("/", index_handler, "index");
+    router.get("/:config", handler, "config");
+
+    fn index_handler(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, INDEX)))
     }
 
-    let _server = Iron::new(hello_world).http("localhost:3000").unwrap();
-    println!("On 3000");
+    fn handler(req: &mut Request) -> IronResult<Response> {
+        let ref config = req.extensions.get::<Router>().unwrap().find("config").unwrap();
+        Ok(Response::with((status::Ok, format!("Hello World! {}", config))))
+    }
+
+    let _server = Iron::new(router).http("localhost:3000").unwrap();
+    println!("Listening on port 3000");
 }
 
 // Local Variables:
